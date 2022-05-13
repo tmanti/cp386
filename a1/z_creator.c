e4db65537bcf83251e66be2e7b4bbcd89498329f
tmanti <tmanti17@gmail.com>
2022-05-12 20:17:44 -0400

    initial commit

---
+++ a1/z_creator.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char* args[]){
+    int sleep_timer = 50;
+
+    pid_t child_pid = fork();
+  
+    // Parent process 
+    if (child_pid > 0)
+        sleep(sleep_timer);
+  
+    // Child process
+    else
+        exit(0);
+  
+    return 0;
+}
